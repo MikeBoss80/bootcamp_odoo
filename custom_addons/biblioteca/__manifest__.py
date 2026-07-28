e7b6106aa0ce6b30e42be50e62b5f638474ee527
Miguel Angel Bolivar Parra <miguel.bolivar3000@gmail.com>
2026-07-28 12:37:54 +0000

    feat: agregar módulo Biblioteca con manifest y modelos iniciales para gestión de libros

---
+++ custom_addons/biblioteca/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Biblioteca",
+    "version": "17.0.1.0.0",
+    "category": "Services",
+    "summary": "Gestión básica de biblioteca",
+    "description": """
+        Módulo para gestionar libros y autores.
+    """,
+    "author": "Miguel Bolivar",
+    "website": "",
+    "license": "LGPL-3",
+
+    "depends": [
+        "base",
+    ],
+
+    "data": [
+    ],
+
+    "demo": [
+    ],
+
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
